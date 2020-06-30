553bf64a9d50422d17b7c8d9159dbc968904c84f
代圣达 <dsd13502@gmail.com>
2020-06-30 14:26:39 +0800

    初始化项目

---
+++ src/main/kotlin/com/mrcd/Converter.kt
@@ -0,0 +1,15 @@
+package com.mrcd
+
+/**
+ *
+ * Create by im_dsd 2020/6/30 13:35
+ */
+class Converter {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World!")
+        }
+    }
+}
